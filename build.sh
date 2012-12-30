844a1542fc7403c643d75b049e8a8cb4dcef6429
Graham Cox <graham@grahamcox.co.uk>
2012-12-30 14:18:37 +0000

    Enough to get things building and producing a working - albeit useless - executable

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+pushd `dirname $0`
+TOPDIR=`pwd`
+BUILDDIR=$TOPDIR/output
+INSTALLDIR=$BUILDDIR/install
+
+rm -rf $BUILDDIR
+mkdir $BUILDDIR
+
+pushd $BUILDDIR
+
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -Wno-dev --warn-uninitialized --warn-unused-vars  $@ $TOPDIR || exit 1
+make || exit 2
+make test || exit 3
+make install || exit 4
+
+popd
+
+popd
