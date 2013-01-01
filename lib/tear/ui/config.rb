e62f2afacab34a2dde50bc18a50c97411eef10b1
Graham Cox <graham@grahamcox.co.uk>
2013-01-01 20:49:24 +0000

    The layout of the config dialog

--- lib/tear/ui/config.rb
+++ lib/tear/ui/config.rb
@@ -1,13 +1,56 @@
 require "gtk2"
+require "tear/logger"
 
 module UI
-    class Config < Gtk::Window
+    # Wrapper around the configuration dialog
+    class Config
         def initialize
-            super
+            @dialog = Gtk::Dialog.new "Tear - Config", 
+                nil, 
+                nil, 
+                [Gtk::Stock::OK, Gtk::Dialog::RESPONSE_OK], 
+                [Gtk::Stock::CANCEL, Gtk::Dialog::RESPONSE_CANCEL]
 
-            set_title "Tear - Config"
-            set_default_size 250, 200
-            set_window_position Gtk::Window::POS_CENTER
+            @dialog.set_deletable false
+            @grid = Gtk::Table.new(3, 2)
+            @dialog.vbox.add @grid
+
+            @baseDirEntry = Gtk::Entry.new
+            addLabelledWidget("Base Dir", @baseDirEntry, 0);
+
+            @filenameEntry = Gtk::Entry.new
+            addLabelledWidget("Filename", @filenameEntry, 1);
+
+            @dialog.signal_connect("response") { |dialog, response|
+                if response == Gtk::Dialog::RESPONSE_OK
+                    onSave
+                else
+                    onCancel
+                end
+            }
+        end
+
+        # Show the dialog
+        def show
+            @dialog.show_all
+        end
+
+        private 
+        # Handle when the Save button is pressed
+        def onSave
+            $log.info "Saving config changes"
+        end
+
+        # Handle when the Cancel button is pressed
+        def onCancel
+            $log.info "Cancelling config changes"
+        end
+
+        # Helper to add a labelled widget to the dialog
+        def addLabelledWidget(labelText, field, row)
+            label = Gtk::Label.new(labelText)
+            @grid.attach(label, 0, 1, row, row + 1)
+            @grid.attach(field, 1, 2, row, row + 1)
         end
     end
 end
